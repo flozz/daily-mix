8e281051837954adf427643002fed0c66259bac7
Fabien LOISON <flo@flogisoft.com>
2024-07-29 14:00:46 +0200

    feat: Implemented a custom urlencode method to handle lists

---
+++ flozz_daily_mix/helpers.py
@@ -0,0 +1,24 @@
+import urllib.parse
+
+
+def custom_urlencode(params):
+    """A custom version of urllib.parse.urlencode that encodes lists and tuple
+    by repeating the key for each item.
+
+    >>> custom_urlencode({"foo": ["bar", "baz", "pwet"]})
+    'foo=bar&foo=baz&foo=pwet'
+    >>> custom_urlencode({"foo": ("bar", "baz", "pwet")})
+    'foo=bar&foo=baz&foo=pwet'
+    >>> custom_urlencode({"foo": "bar", "fizz": "buzz"})
+    'foo=bar&fizz=buzz'
+    """
+    components = []
+
+    for key, value in params.items():
+        if type(value) in [list, tuple]:
+            for item in value:
+                components.append(urllib.parse.urlencode({key: item}))
+        else:
+            components.append(urllib.parse.urlencode({key: value}))
+
+    return "&".join(components)
